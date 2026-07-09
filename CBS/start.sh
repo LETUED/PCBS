ad97ffa6e676ca030c7ef4c2cd10afe1b80114c1
LETUED <woo9910203626@gmail.com>
2026-07-09 22:51:05 +0900

    chore: gitignore 추가, IDE 설정·pyc 추적 해제, 실행 스크립트 경로 정리

--- CBS/start.sh
+++
@@ -1,3 +0,0 @@
-python /Users/jeondonghwan/PycharmProjects/CBS/CBS/app.py
-cd /Users/jeondonghwan/PycharmProjects/CBS/front
-npm start
--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
-cd /Users/jeondonghwan/Desktop/PCBS/CBS
-python app.py
-cd /Users/jeondonghwan/Desktop/PCBS/front
+#!/bin/sh
+# 백엔드(Flask)와 프론트(React dev server)를 함께 실행
+cd "$(dirname "$0")"
+python -m CBS.app &
+cd front
 npm start
